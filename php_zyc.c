58568f51762991eba0f561f4bec8c454d15bdcad
Yuncong Zhang <njitzyc@gmail.com>
2012-12-21 05:34:17 +0300

    use parsed len variable

--- php_zyc.c
+++ php_zyc.c
@@ -28,18 +28,18 @@ ZEND_GET_MODULE(zyc)
 PHP_FUNCTION(my_foo)
 {
   char *phone_number = NULL;
-  unsigned int len;
+  unsigned int len = 0;
   if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &phone_number, &len) == FAILURE)
   {
     return;
   }
 
-  if(strlen(phone_number) < PHONE_NUMBER_LEN - 1)
+  if(len < PHONE_NUMBER_LEN - 1)
   {
     php_printf("zyc's phone number is: '%s'\n", phone_number);
   }
   else
   {
-    php_printf("Phone Number must be shorter than %d characters\n", PHONE_NUMBER_LEN - 1);
+    php_printf("Phone Number must be shorter than %d characters. Input contains %d characters\n", PHONE_NUMBER_LEN - 1, len);
   }
 }
